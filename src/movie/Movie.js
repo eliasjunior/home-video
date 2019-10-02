5d49fef9fbc1be0dac29c577ddbb93f8ab335860
Elias Moreira <elias_moreirajunior@mastercard.com>
2019-10-02 21:50:51 +0100

    some changes

--- src/movie/Movie.js
+++ src/movie/Movie.js
@@ -5,44 +5,54 @@ import './movie.css';
 
 const { API_URL } = constants
 class Movie extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            loadedFailed: false
-        }
+  constructor(props) {
+    super(props)
+    this.state = {
+      loadedFailed: false
     }
-    onErrorHandle = () => {
-        this.setState({ loadedFailed: true })
+  }
+  onErrorHandle = () => {
+    this.setState({ loadedFailed: true })
+  }
+  renderVideo = () => {
+    const { videoPath } = this.props;
+    console.log('EMA CHORANDO', videoPath)
+    const getSubFromVideoUrl = () => {
+      const removeSrtFromBegin = videoPath.slice(15); //videos/movies_, temporary
+      return removeSrtFromBegin.slice(0, removeSrtFromBegin.length - 3) + 'vtt'
     }
-    renderVideo = () => {
-        const { videoPath } = this.props;
-        const sub = videoPath && videoPath.slice(0, videoPath.length - 3) + 'vtt'
-        if (this.state.loadedFailed) {
-            return <div>
-                <div className='message-error'>Video Failed to load</div>
-                <Footer></Footer>
-            </div>
-        } else {
-            return <video className="video-guy" 
-                onError={this.onErrorHandle} preload="metadata"
-                controls
-                id="videoPlayer"
-                autoPlay={true}>
-                <source src={API_URL + videoPath} type="video/mp4"></source>
-                {/* <track src={API_URL + sub} 
-                    label="English" 
-                    kind="subtitles" 
-                    srcLang="en" default>
-                </track> */}
-            </video>
-        }
+    console.log('SUBTITLE --> ', getSubFromVideoUrl())
+    const sub = videoPath && getSubFromVideoUrl();
+    if (this.state.loadedFailed) {
+      return <div>
+        <div className='message-error'>Video Failed to load</div>
+        <Footer></Footer>
+      </div>
+    } else {
+      return (
+        <video className="video-guy"
+          onError={this.onErrorHandle} preload="metadata"
+          controls
+          id="videoPlayer"
+          autoPlay={true}>
+          <source src={API_URL + videoPath} type="video/mp4"></source>
+          <track src={sub}
+            label="English"
+            kind="subtitles"
+            srcLang="en" default>
+          </track>
+        </video>
+      )
     }
-    render() {
-        return <div>
-            {this.renderVideo()}
-        </div>
-    }
+  }
+  render() {
+    return <div>
+      {this.renderVideo()}
+    </div>
+  }
 }
+
+
 
 
 export default Movie
