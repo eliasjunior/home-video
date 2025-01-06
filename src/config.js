5e597163ece85351b2007b3c56d697b85abaaa29
eliasjunior <emcjunior@gmail.com>
2025-01-06 18:35:01 +0100

    add get Ip address function

--- src/config.js
+++ src/config.js
@@ -1,19 +1,35 @@
-const { NODE_ENV, REACT_APP_SERVER_HOST, REACT_APP_SERVER_PROTOCOL } =
-  process.env;
+const { NODE_ENV, REACT_APP_SERVER_PROTOCOL } = process.env;
 
 export default function config() {
   const result = {};
-  console.log(`React end-point => ${REACT_APP_SERVER_HOST}`);
-  console.log(`Env => ${NODE_ENV }`);
+  const host = getLocalIPAddress();
+  console.log(`React end-point => ${getLocalIPAddress()}`);
+  console.log(`Env => ${NODE_ENV}`);
   if (NODE_ENV === "production") {
     result.PROTOCOL = REACT_APP_SERVER_PROTOCOL;
     result.PORT = process.env.PORT || 8080;
-    result.host = REACT_APP_SERVER_HOST;
+    result.host = host;
   } else {
     result.PROTOCOL = "http";
     result.PORT = 8080;
-    result.host = REACT_APP_SERVER_HOST; // testing purposes, pointing to prod
+    result.host = host; // testing purposes, pointing to prod
   }
   result.SERVER_URL = `${result.PROTOCOL}://${result.host}:${result.PORT}`;
   return result;
+}
+
+function getLocalIPAddress() {
+  const os = require("os");
+
+  const networkInterfaces = os.networkInterfaces();
+  for (const interfaceName in networkInterfaces) {
+    for (const iface of networkInterfaces[interfaceName]) {
+      // Look for the IPv4, non-internal address
+      if (iface.family === "IPv4" && !iface.internal) {
+        console.log("Local Host IP:", iface.address);
+        return iface.address;
+      }
+    }
+  }
+  return "127.0.0.1"; // Fallback to localhost
 }
